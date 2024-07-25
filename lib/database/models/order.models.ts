306d5e0a5ddd89b6dbbd5c5ec293fb00c3caeda0
Aryan-yadav15 <a2003yadav@gmail.com>
2024-07-25 22:27:52 +0530

    mongoose

--- lib/database/models/order.models.ts
+++ lib/database/models/order.models.ts
@@ -1,4 +1,4 @@
-import { Schema, model, models, Document } from 'mongoose'
+import mongoose, { Schema, model, models, Document } from 'mongoose'
 
 export interface IOrder extends Document {
   createdAt: Date
@@ -47,6 +47,6 @@ const OrderSchema = new Schema({
   },
 })
 
-const Order = models.Order || model('Order', OrderSchema)
+const Order = mongoose.models.Order || mongoose.model('Order', OrderSchema)
 
 export default Order
